b5779c1864cc6bb29a687bcea472b4d0ef0d6e75
braulioalmonte <brauliox2112@hotmail.com>
2025-03-15 11:19:47 -0400

    fix for deprecated ft.UserControl and attempted to inherit the control from the ft.Control Class

--- flet_timer/flet_timer.py
+++ flet_timer/flet_timer.py
@@ -1,19 +1,56 @@
 import threading
-
 import flet as ft
 from interval_timer import IntervalTimer
+from flet.core.control import Control
+from flet.core.ref import Ref
+from typing import Any, Optional
+from flet.core.types import OptionalControlEventCallable
+from flet.core.control import OptionalNumber
 
-
-class Timer(ft.UserControl):
-    def __init__(self, name="Timer", interval_s=1, callback=None, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+class Timer(ft.Control):
+    def __init__(
+            self, 
+            name: Optional[str] = None, 
+            interval_s: OptionalNumber = None, 
+            callback : OptionalControlEventCallable = None,
+            #
+            # Control
+            #
+            ref: Optional[Ref] = None,
+            data: Any = None
+            ):
+    
+        Control.__init__(
+            self,
+            ref=ref,
+            data=data,
+        )
         self.name = name
         self.interval_s = interval_s
         self.callback = callback
         self.active = False
         self.th = threading.Thread(target=self.tick, daemon=True)
+
         if callback is None:
             self.callback = lambda: print(f"Timer {self.name} ticked")
+
+    # name
+    @property
+    def name(self) -> Optional[str]:
+        return self._get_attr("name")
+
+    @name.setter
+    def name(self, value: Optional[str]):
+        self._set_attr("name", value)
+
+    # interval
+    @property
+    def interval_s(self) -> OptionalNumber:
+        return self._get_attr("interval_s")
+
+    @interval_s.setter
+    def interval_s(self, value) -> OptionalNumber:
+        self._set_attr("interval_s", value)
 
     def did_mount(self):
         self.start()
@@ -40,3 +77,9 @@ class Timer(ft.UserControl):
     def will_unmount(self):
         self.stop()
         super().will_unmount()
+
+    def before_update(self):
+        super().before_update()
+
+    def _get_control_name(self):
+        return "timer"
